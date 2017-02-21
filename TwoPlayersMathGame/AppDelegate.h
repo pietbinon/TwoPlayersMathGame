f971a06a7ac8b43d5e7a4f5e6f6c524a0e095c2a
pietbinon <pierrebinon@icloud.com>
2017-02-21 15:32:55 -0500

    Initial Commit

---
+++ TwoPlayersMathGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoPlayersMathGame
+//
+//  Created by Pierre Binon on 2017-02-21.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwoPlayersMathGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwoPlayersMathGame
+//
+//  Created by Pierre Binon on 2017-02-21.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
